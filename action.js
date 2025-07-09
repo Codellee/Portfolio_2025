8d273e146fab2bfb8246ea8e28358f26f0498b83
Codellee <134716768+Codellee@users.noreply.github.com>
2025-07-09 22:32:44 +0800

    carousel

--- action.js
+++ action.js
@@ -1,20 +1,31 @@
-let slideIndex = 0;
+/** let slideIndex = 0;
 showSlides();
 
-function showSlides() {
-    let i;
+function showSlides (){
+let i;
     let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
     setTimeout(showSlides, 4000);
+} */
+
+
+function myFunction(imgs) {
+    var expandImg = document.getElementsByClassName("expandedImg");
+    expandImg.scr = imgs.scr;
+    expandImg.parentElement.style.display = "block";
 }
 
+const carousel = document.getElementsById("slider");
+let scrollSpeed = 1;
+
+function autoScroll(){
+    if (carousel.scrollLeft >= carousel.scrollWidth / 2) {
+        carousel.scrollLeft = 0;
+    } else {
+        carousel.scrollLeft += scrollSpeed;
+    }
+    requestAnimationFrame(autoScroll);
+}
+autoScroll();
